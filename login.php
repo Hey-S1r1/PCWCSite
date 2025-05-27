03dab6431c71f131203763cae5d9383cc24dbcb8
Hey-Sri <vallabhsr@gmail.com>
2025-05-27 00:04:19 -0400

    Basic email checking

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="test.php" method="get">
+        <label>Email:</label><br>
+        <input type="text" name="email"><br>
+    </form>
+</body>
+</html>
+<?php
+    if (isset($_GET["email"]) && filter_var($_GET["email"], FILTER_VALIDATE_EMAIL) && explode("@", $_GET["email"])[1] == "purdue.edu") {
+        $email = $_GET["email"];
+        echo $email;
+    } else {
+        echo "Please enter a purdue.edu email address";
+    }
+    
+?>

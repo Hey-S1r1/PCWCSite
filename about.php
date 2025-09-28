5419b864a5c47461aee3626143867816791dab28
Hey-S1r1 <vallabhsr@gmail.com>
2025-09-28 15:03:51 -0400

    Basics finished!

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
     <body>
         <div class="container-fluid">
             <nav id="homeNav" class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Portfolio</a>
+                <a class="navbar-brand" href="#">Purdue Creative Writing Club</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
@@ -24,32 +24,6 @@
                   </ul>
                 </div>
             </nav>
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              </ol>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="https://via.placeholder.com/800x400" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="https://placecats.com/g/800/400" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="https://placedog.net/800/400" class="d-block w-100" alt="...">
-                </div>
-              </div>
-              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
             <h1 class="text-center">About Us</h1>
             <p style="column-count: 2;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus faucibus elementum justo sit amet tempor. Aliquam id odio ut orci egestas volutpat eu et sapien. Ut fermentum scelerisque velit, vel interdum massa dignissim non. Donec ut nibh justo. Suspendisse maximus nisl sit amet mauris condimentum, in commodo erat venenatis. Vivamus luctus consequat nunc, sit amet congue elit congue a. Proin porttitor dolor id lectus pretium, a auctor nunc ultrices. In a vulputate purus. Integer ac dolor scelerisque, dignissim lectus sit amet, rutrum dolor. Nulla id ullamcorper leo. Aliquam vitae risus egestas, dignissim nulla a, finibus tellus.
               Cras venenatis viverra est eu sollicitudin. Integer et odio in justo ullamcorper semper. Maecenas finibus in felis eu cursus. Etiam condimentum sem id turpis tempor dapibus. Vestibulum et tristique turpis. Nulla facilisi. Nullam tempus ullamcorper risus, nec ornare sapien suscipit vel. Donec sagittis commodo lectus, sed congue arcu. Donec condimentum risus nec sapien convallis pretium. Duis consequat vel ipsum vel porta. Praesent malesuada consequat nisl, at sagittis dolor finibus ut. Sed dignissim felis nisl, tristique blandit neque faucibus sit amet. In id tempus sem. Morbi vel arcu in ligula rutrum imperdiet.</p>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Purdue CWC: Home Page</title>
+        <title>Purdue Creative Writing Club: Home Page</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -9,7 +9,7 @@
     <body>
       <div class="container-fluid">
         <nav id="homeNav" class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Portfolio</a>
+          <a class="navbar-brand" href="#">Purdue Creative Writing Club</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -25,9 +25,16 @@
           </div>
         </nav>
         <div class="jumbotron">
-          <h1>Welcome to Purdue Creative Writing Club's Website!</h1>
+          <h1>Welcome to the Purdue Creative Writing Club's Website!</h1>
           <p>We hope you enjoy your stay.</p>
         </div>
+        <h1>Who are we?</h1>
+        <p>We are the Creative Writng Club at Purdue!</p>
+        <h2>What do we want?</h2>
+        <p>Funding!</p>
+        <h2>When do we want it?</h2>
+        <p>Now!</p>
+        <iframe src="https://calendar.google.com/calendar/embed?src=cwc.purdue%40gmail.com&ctz=America%2FIndiana%2FIndianapolis" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
     </body>
 </html>
